3b86602b37d1f8ec1f57e9949eecb16ce59367a8
fmaylinch <ferranmaylinch@gmail.com>
2018-09-13 13:08:29 +0200

    Add ProductCatalog program that writes CSV

---
+++ src/main/java/tech/bts/productcatalog/ProductCatalog.java
@@ -0,0 +1,36 @@
+package tech.bts.productcatalog;
+
+import java.io.PrintWriter;
+import java.util.*;
+
+public class ProductCatalog {
+
+    public static void main(String[] args) throws Exception {
+
+        Product p1 = new Product("iPhone X", 1000, 50);
+        Product p2 = new Product("Macbook Pro", 1500, 30);
+        Product p3 = new Product("Mouse", 30, 100);
+
+        List<Product> products = new ArrayList<Product>();
+        products.add(p1);
+        products.add(p2);
+        products.add(p3);
+
+        PrintWriter writer = new PrintWriter("products.csv");
+
+        // Name,Price,Units
+        // iPhone X,1000,50
+        // Macbook Pro,1500,30
+
+        writer.println("Name,Price,Units");
+
+        for (int i = 0; i < products.size(); i++) {
+            Product product = products.get(i);
+            writer.println(product.name + "," + product.price + "," + product.unitsInStock);
+        }
+
+        writer.close();
+
+        System.out.println("File written");
+    }
+}
